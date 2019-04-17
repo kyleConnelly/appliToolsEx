c15806cf11c2c0651506b574d4bfa1389c1311c5
Kyle Connelly <kyleconnelly@Kyles-MacBook-Pro.local>
2019-04-17 14:59:40 -0500

    environment variables

--- appliToolsEx/src/test/java/com/cbt/appliToolsEx/AppTest.java
+++ appliToolsEx/src/test/java/com/cbt/appliToolsEx/AppTest.java
@@ -3,36 +3,55 @@ package com.cbt.appliToolsEx;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.RectangleSize;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class ApplitoolsTest extends TestCase {
+    private String username = System.getenv("CBTUSRNAME"), authkey = System.getenv("CBTAUTH"), score = "fail",
+            hubUrl = "http://" + username + ":" + authkey + "@hub.crossbrowsertesting.com:80/wd/hub";
+    private CBTHelper cbt;
+    private RemoteWebDriver driver;
+    private Eyes eyes;
+
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        cbt = new CBTHelper(username, authkey, false);
+        caps.setCapability("name", "KC Applitools Example");
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("screenResolution", "1366x768");
+
+        eyes = new Eyes();
+        eyes.setApiKey(System.getenv("APLITOOLS"));
+
+        // create a new RemoteWebDriver object
+        driver = new RemoteWebDriver(new URL(hubUrl), caps);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testApp() {
+        // open a new instance of eyes
+        eyes.open(driver, "CrossBrowserTesting", "My first Selenium Java test!",
+                new RectangleSize(800, 600));
+
+        // navigate to the webpage we'd like to capture
+        driver.get("https://crossbrowsertesting.com/visual-testing");
+
+        // Visual checkpoint
+        eyes.checkWindow("Visual Testing");
+
+        eyes.close();
+        score = "pass";
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void tearDown() {
+        cbt.setSessionId(driver.getSessionId().toString());
+        cbt.setScore(score);
+        if (driver != null)
+            driver.quit();
+        eyes.abortIfNotClosed();
     }
 }
